3526ca785668c62aaa9a19f8c3ae5ede6b06b38e
Nazeeb <nazeebmail@gmail.com>
2023-04-28 18:06:50 +0530

    28april

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import Button from '@mui/material/Button';
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    neutral: {
+      main: '#64748B',
+      contrastText: '#fff',
+    },
+  },
+});
+
 
 
 function App() {
   return (
-    <div>hello 
+    <div>
+      <ThemeProvider theme={theme}>
+      <Button variant="contained" >
+        Login
+        </Button>
+        </ThemeProvider>
     </div>
   );
 }
